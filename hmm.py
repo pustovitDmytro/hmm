e26aaecdccfcf03cfc12567912f385a9a0311056
dmytroPustovit <pustic@gmail.com>
2017-02-21 23:16:20 +0200

    coins (wrong probabilities)

--- hmm.py
+++ hmm.py
@@ -2,70 +2,84 @@
 import numpy
 from abc import ABCMeta, abstractmethod
 def main():
-	N=10
+	N=100
 	obs = numpy.random.binomial(1,0.3,N)
-	model = HMM(2,N,obs)
-	model.Forward()
-	model.BackWard()
+	model = HMM(2,2,obs)
+	model.show()
 	model.Baum_Welch()
 	model.show()
 
 class MM():
 	__metaclass__ = ABCMeta
 	def __init__(self, n,t):
-          		self._N = n
-          		self._T = t
-          		self._A = [[1./n for i in range(self._N)]for j in range(self._N)]
+          		self.N = n
+          		self.T = t
+          		self.A = [[1./n for i in range(self.N)]for j in range(self.N)]
 class HMM(MM):
-	def __init__(self, n , t, ob):
-		super(HMM,self).__init__(n,t)
-		self._B = [[1./n for i in range(self._N)]for t in range(self._T)]
-		self._obs = ob       
+	def __init__(self, n , l, ob):
+		super(HMM,self).__init__(n,len(ob))
+		self.L = l
+		self.B = [[1./n for i in range(self.N)]for t in range(self.L)]
+		self.obs = ob       
 	
 	def show(self): 
-		print('A:\n',self._A)
-		print('B:\n',self._B)
-	
+		print('A:\n',self.A)
+		print('B:\n',self.B)
+
 	def Forward(self):
-		alfa = [[0.5 if j==0 else 0 for i in range(self._N)] for j in range(self._T)]
-		for t in range(1,self._T):
-			for i in range(self._N):
-				for j in range(self._N):
-					alfa[t][i]+=alfa[t-1][j]*self._A[i][j]*self._B[t][j]
+		alfa = [[0.5 if j==0 else 0 for i in range(self.N)] for j in range(self.T)]
+		for t in range(1,self.T):
+			for i in range(self.N):
+				for j in range(self.N):
+					alfa[t][i]+=alfa[t-1][j]*self.A[i][j]*self.B[self.obs[t]][j]
 		self.alfa = alfa
 
 	def BackWard(self):
-		beta = [[1 if j==self._T-1 else 0 for i in range(self._N)] for j in range(self._T)]
-		for t in range(self._T-1,0,-1):
-			for i in range(self._N):
-				for j in range(self._N):
-					beta[t-1][i]+=beta[t][j]*self._A[i][j]*self._B[t][j]
+		beta = [[1 if j==self.T-1 else 0 for i in range(self.N)] for j in range(self.T)]
+		for t in range(self.T-1,0,-1):
+			for i in range(self.N):
+				for j in range(self.N):
+					beta[t-1][i]+=beta[t][j]*self.A[i][j]*self.B[self.obs[t]][j]
 		self.beta = beta;
 
 	def Baum_Welch(self):
-		v = [1,0]
-		gama = [[[self.beta[t+1][j]*self._A[i][j]*self._B[t][j]*self.alfa[t][i]for i in range(self._N)] for j in range(self._N)] for t in range(self._T)] 
-		A = self._A
-		B = self._B
-		for i in range(self._N):
-			for j in range(self._N):
-				s = 0
-				Sum = 0;
-				for t in range(self._T):
-					s+=gama[i][j][t]
-				for i1,t1 in zip(range(self._N),range(self._T)):
-					Sum+=gama[i1][j][t1]
-				A[i][j] = s/Sum;
+		for iter in range(100):
+			self.Forward()
+			self.BackWard()
+			ksi = [[[self.beta[t+1][j]*self.A[i][j]*self.B[self.obs[t+1]][j]*self.alfa[t][i] for i in range(self.N)] for j in range(self.N)] for t in range(self.T-1)]
+			gama  = [[self.beta[t][i]*self.alfa[t][i] for i in range(self.N)] for t in range(self.T)]
+			for t in range(self.T):
+				s1=0
+				for j in range(self.N):
+					s1+=self.beta[t][j]*self.alfa[t][j]
+				for i in range(self.N):
+					gama[t][i] = gama[t][i]/s1
+			for t in range(self.T-1):
+					s2=0
+					for i,j in zip(range(self.N),range(self.N)):
+						s2+=self.beta[t+1][j]*self.A[i][j]*self.B[self.obs[t+1]][j]*self.alfa[t][i]
+					for i,j in zip(range(self.N),range(self.N)):
+						ksi[t][j][i] = ksi[t][j][i]/s2
+			for i in range(self.N):
+				for j in range(self.N):
+					s1 = 0
+					s2 = 0
+					for t in range(self.T-1):
+						s1+=gama[t][i]
+						s2+=ksi[t][j][i]
+					self.A[i][j] = s2/s1
+	
+			for j in range(self.N):
+				for k in range(self.L):
+					s1=0
+					s2=0
+					for t in range(self.T-1):
+						s1+=gama[t][j]
+						if (self.obs[t] == k): s2+=gama[t][j]
+					self.B[j][k] = s2/s1
 
-		for j in range(self._N):
-			for t in range(self._T):
-				for i1,t1 in zip(range(self._N),range(self._T)):
-					Sum+=gama[i1][j][t1]
-				for i1,t1 in zip(range(self._N),range(self._T)):
-					if self._obs[t1] Sum+=gama[i1][j][t1]
-
-
-
+			print("Iteration",iter)
+			self.show()
 
 if __name__ == '__main__':
 	main()
