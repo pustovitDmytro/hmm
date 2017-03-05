044ec3646c615e26bec92b99dc0b0e56a424f9fd
dmytroPustovit <pustic@gmail.com>
2017-03-05 12:41:21 +0200

    baum-welch correct by MIT

--- hmm.py
+++ hmm.py
@@ -4,10 +4,10 @@ from abc import ABCMeta, abstractmethod
 def main():
 	#N=10
 	#obs = numpy.random.binomial(1,0.3,N)
-	obs = [0,2,2]
-	#obs = [2,0,0,2,1,2,1,1,1,2,1,1,1,1,1,2,2,0,0,1]
 	
-	model = HMM(3,3,obs)
+	obs = [2,0,0,2,1,2,1,1,1,2,1,1,1,1,1,2,2,0,0,1]
+	
+	model = HMM(2,3,obs)
 	model.Baum_Welch()
 	#model.Baum_Welch()
 
@@ -16,15 +16,15 @@ class MM():
 	def __init__(self,n,t):
           		self.N = n
           		self.T = t
-          		self.A = [[.1,.4,.5],[.4,0,.6],[0,.6,.4]]#[[.5,.5],[.5,.5]]
-          		self.Pi = [1,0,0]
+          		self.A = [[.5,.5],[.5,.5]]
+          		self.Pi = [.5,.5]
           		#self.A = [[1./n for i in range(self.N)]for j in range(self.N)]
 class HMM(MM):
 	def __init__(self, n , l, ob):
 		super(HMM,self).__init__(n,len(ob))
 		self.L = l
 		#self.B = [[1./n for i in range(self.N)]for t in range(self.L)]
-		self.B = [[.6,.2,.2],[.2,.6,.2],[.2,.2,.6]]#[[.4,.1,.5],[0.1,.5,.4]]
+		self.B = [[.4,.1,.5],[0.1,.5,.4]]
 		self.obs = ob       
 	
 	def show(self): 
@@ -47,25 +47,28 @@ class HMM(MM):
 				for j in range(self.N):
 					beta[t-1][i]+=beta[t][j]*self.A[i][j]*self.B[j][self.obs[t]]
 		self.beta = beta;
-	def perevirka(self,ksi):
+
+	def perevirka(self,ksi,gama):
 		print("perevirka:")
 		for t in range(self.T-1):
-			print(t)
-			sum=0
-			for q in range(self.N):
-				for s in range(self.N):
-					sum+=ksi[t][q][s]
-			print("sum: ",sum)
+			print("t",t)
+			print("gama = ", gama[t])
+			print("ksi ",ksi[t])
 			
 	def Baum_Welch(self):
-		for iter in range(2):
+		for iter in range(20):
 			print("Iteration",iter)
 			self.show()
 			self.Forward()
 			self.BackWard()
-			print("alfa: ",self.alfa)
-			print("beta: ",self.beta)
-			ksi = [[[self.beta[t+1][j]*self.A[i][j]*self.B[j][self.obs[t+1]]*self.alfa[t][i] for j in range(self.N)] for i in range(self.N)] for t in range(self.T-1)]
+			#print("alfa: ",self.alfa)
+			#print("beta: ",self.beta)
+			ksi = [[[self.alfa[t][q]*self.A[q][s]*self.B[s][self.obs[t+1]]*self.beta[t+1][s] for q in range(self.N)]for s in range(self.N)] for t in range(self.T-1)]
+			for t in range(self.T-1):
+				for q in range(self.N):
+					for s in range(self.N):
+						ksi[t][q][s] = self.alfa[t][q]*self.A[q][s]*self.B[s][self.obs[t+1]]*self.beta[t+1][s]
+
 			gama  = [[self.beta[t][i]*self.alfa[t][i] for i in range(self.N)] for t in range(self.T)]
 			pro = 0
 			for i in range(self.N):
@@ -76,17 +79,19 @@ class HMM(MM):
 					s1+=self.beta[t][j]*self.alfa[t][j]
 				for i in range(self.N):
 					gama[t][i] = gama[t][i]/s1
-			print("s1 = ",s1,"pro= ",pro)
 			for t in range(self.T-1):
 				s2=0
-				for i,j in zip(range(self.N),range(self.N)):
-					s2+=self.beta[t+1][j]*self.A[i][j]*self.B[j][self.obs[t+1]]*self.alfa[t][i]
-				for i,j in zip(range(self.N),range(self.N)):
-					ksi[t][i][j] = ksi[t][i][j]/s2
-			print("s2 = ",s2)
+				for q in range(self.N):
+					for s in range(self.N):
+						s2+= self.alfa[t][q]*self.A[q][s]*self.B[s][self.obs[t+1]]*self.beta[t+1][s]
+				for q in range(self.N):
+					for s in range(self.N):
+						ksi[t][q][s] = ksi[t][q][s]/s2
+				
+			#print("s1 = ",s1,"pro= ",pro, "s2 = ",s2)
 			#print("gama:\n",gama)
 			#print("ksi\n",ksi)
-			#self.perevirka(ksi)
+			#self.perevirka(ksi,gama)
 
 			for i in range(self.N):
 				self.Pi[i] = gama[0][i];
@@ -95,14 +100,12 @@ class HMM(MM):
 					s2 = 0
 					for t in range(self.T-1):
 						s2+=ksi[t][i][j]
-					print("i: ",i,"j",j,"ksi",s2)
 					self.A[i][j] = s2
 
 			for i in range(self.N):
 				s1=0
 				for t in range(self.T-1):
 						s1+=gama[t][i]
-				print("i: ",i,"gama",s1)
 				for j in range(self.N):
 					self.A[i][j] =self.A[i][j]/s1
 
--- test2.py
+++ test2.py
@@ -1,14 +1,1 @@
-s=0
-i=0
-while True:
-	i+=1
-	if(i<10):
-		s+=1
-	elif(i<100):
-		s+=2
-	elif(i<1000):
-		s+=3
-	if s==2775:
-		break
-	print(s)
-	print(i)
+
