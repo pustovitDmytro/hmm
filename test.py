7b727ff11b9e89bd885fb7006a33afbacc59d042
dmytroPustovit <pustic@gmail.com>
2017-03-07 19:51:44 +0200

    test: reading observations from txt file

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-f = open("A.txt")
-A = f.read()
-print(A)
+def fromFile(name):
+	N=3
+	f = open(name)
+	A = f.readline().split('\t')
+	a = []
+	for i in A:
+		a.append(float(i))
+	return a
+print(fromFile("data.txt"))
